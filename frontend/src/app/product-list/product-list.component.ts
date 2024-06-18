1e07c38fea9bfdd51360c34b53f2880b3a38f61d
Cesar <cperezcampo30@gmail.com>
2024-06-18 16:28:55 -0500

    productList created with database

--- frontend/src/app/product-list/product-list.component.ts
+++ frontend/src/app/product-list/product-list.component.ts
@@ -2,17 +2,24 @@ import { Component, inject } from '@angular/core';
 import { SchemaProductsComponent } from '../schema-products/schema-products.component';
 import { ProductsService } from '../service/products.service';
 import { RouterLinkWithHref } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [SchemaProductsComponent, RouterLinkWithHref],
+  imports: [SchemaProductsComponent, RouterLinkWithHref, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  
+  products: any[] = [];
   private productsService = inject(ProductsService);
 
-  products = this.productsService.products;
+  // products = this.productsService.products; 
+  ngOnInit(): void {
+    this.productsService.productList().subscribe(data => {
+      this.products = data;
+    });
+    
+  }
 }
--- frontend/src/app/service/products.service.ts
+++ frontend/src/app/service/products.service.ts
@@ -1,10 +1,31 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject } from '@angular/core';
 import { Products } from '../model/products';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private http = inject(HttpClient)
+  
+  productList(name?: string, price?: string): Observable<any>{
+    let params = new HttpParams();
 
+    if (name) {
+      params = params.append('name', name);
+    }
+
+    if (price) {
+      params = params.append('price', price);
+    }
+    return this.http.get("http://localhost:3000/articulos",
+      {
+        params
+      }
+    )
+  }
   constructor() { }
   products = signal([
     { id: 1,
