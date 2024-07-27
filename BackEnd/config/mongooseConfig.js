b13e7ec159612cb0f55aafbf687fcac7a8911e3d
Stiven <stivenpoveda30sistema@gmail.com>
2024-07-26 20:07:08 -0500

    conexionip

--- BackEnd/config/mongooseConfig.js
+++ BackEnd/config/mongooseConfig.js
@@ -1,6 +1,6 @@
 //Modularizacion de la conexion
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb://localhost:27017/prueba-mongodb")
-
+//mongoose.connect("mongodb://localhost:27017/prueba-mongodb")
+mongoose.connect("mongodb://3.23.86.39:27017/prueba-mongodb")
 export default mongoose;
