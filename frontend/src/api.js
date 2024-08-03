76832ee1c67ddaac19bce1ea992a67bf7991b799
Stiven <stivenpoveda30sistema@gmail.com>
2024-08-02 21:32:41 -0500

    genero

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 function obtenerArticulos() {
-    return fetch('http://localhost:3000/api/articulos')
+    return fetch('http://18.217.23.211:3000/api/articulos')
         .then(response => response.json())
         .catch(error => console.error('Error:', error));
 }
