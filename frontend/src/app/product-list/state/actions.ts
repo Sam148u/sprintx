e53dd7a01bc3393e31d895235c3c7c10dc35b391
Stiven <stivenpoveda30sistema@gmail.com>
2024-08-13 12:08:44 -0500

    normal

--- frontend/src/app/product-list/state/actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-
-export const loadProducts = createAction('[Products Page] Load Products');
-export const loadProductsSuccess = createAction(
-    '[Products Page] Load Products Success',
-    props<{ payload: any[] }>()
-);
