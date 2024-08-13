5e5e96939e2dd8ce27aa4d168ab99793342de1ab
Stiven <stivenpoveda30sistema@gmail.com>
2024-08-13 08:59:34 -0500

    normal de nuevo

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
 
 import { routes } from './app.routes';
-import { counterReducer } from './counter/counter.reducer';
-import { CounterEffects } from './counter/counter.effects';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
-    provideStore({ count: counterReducer }),
-    provideEffects([CounterEffects])
-  ]
+  providers: [provideRouter(routes, withComponentInputBinding()),provideHttpClient()]
 };
